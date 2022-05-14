f2ce1775851b5192d83d5f6809d449778c32ac74
Anna <ragebluhane@gmail.com>
2022-05-14 18:24:43 +0700

    начала делать типа журнал

--- ViewModels/MarkViewModel.cs
+++ ViewModels/MarkViewModel.cs
@@ -1,49 +1,76 @@
-﻿using System;
+﻿using nsAPI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Tadar.Helpers;
 using Tadar.Models;
+using Tadar.Views;
 
 namespace Tadar.ViewModels
 {
     class MarkViewModel: BaseViewModel
     {
-        public Mark mark;
+        private List<RegisteredClassroom> classroomsuser = new List<RegisteredClassroom>();
+        private RegisteredClassroom cls;
 
-        public MarkViewModel(Mark mark)
+        public MarkViewModel()
         {
-            this.mark = mark;
+            LoadClasssAsync();
+            //EnterClick = new Command(Enter_Click);
         }
 
-        public string Nameoftest
+
+        public async void LoadClasssAsync()
         {
-            get { return mark.NameOfTest; }
-            set
+            try
             {
-                mark.NameOfTest = value;
-                OnPropertyChanged("Nameoftest");
+                if (api == null)
+                {
+                    throw new Exception("api не создан!!!");
+                }
+                classroomsuser =
+                  await api.GetClassroomsByUserIdAsync(api.MainUser.ID,"3");
+              
+                //  ClasssList = new ObservableCollection<RegisteredClassroom>();
+
+                OnPropertyChanged(nameof(Classrooms));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
-        public DateTime Dateoftest
+        public List<RegisteredClassroom> Classrooms
         {
-            get { return mark.DateOfTest; }
+            get { return classroomsuser; }
             set
             {
-                mark.DateOfTest = value;
-                OnPropertyChanged("Dateoftest");
+                classroomsuser = value;
+                OnPropertyChanged(nameof(Classrooms));
+                // Задаем новый выбранный жлемент из списка.
+                // SelectedClassroom = Classrooms[0];
             }
         }
 
-        public string Markoftest
+        public Command EnterClick { get; }
+        private async void Enter_Click(object ob)
         {
-            get { return mark.MarkOfTest; }
-            set
-            {
-                mark.MarkOfTest = value;
-                OnPropertyChanged("Markoftest");
-            }
+
+            cls = new RegisteredClassroom();
+            cls = (RegisteredClassroom)ob;
+            string iclass = cls.ID;
+            await api.AddStudent(api.MainUser.ID.ToString(), iclass);
+            OnPropertyChanged(nameof(Classrooms));
+            MessageBox.Show(api.MainUser.Name.ToString() + " в классе " + cls.Name.ToString());
+            First.Base_frame.Navigate(new MenuPage());
+
         }
+
+
+
     }
 }
--- Views/MarksPage.xaml.cs
+++ Views/MarksPage.xaml.cs
@@ -21,99 +21,19 @@ namespace Tadar.Views
     /// </summary>
     public partial class marks : Page
     {
-        UserSecViewModel uservm;
+       
         public marks()
         {
             InitializeComponent();
-            sss.DataContext = uservm = new UserSecViewModel();
+            DataContext = new MarkViewModel();
            
-            tas.DataContext = uservm = new UserSecViewModel();
             //Journ.DataContext = custDataTable;
             //journclass.ItemsSource = LoadCollectionData();
 
         }
         
 
-        //void AddTab(object sender, RoutedEventArgs e)
-        //{
-        //    markofclass.Items.Add(new TabItem
-        //    {
-        //        Header = new TextBlock { Text = "Ноутбуки" }, // установка заголовка вкладки
-        //       ///* Content = notesList /*// установка содержимого вкладки
-        //       /////добавление в базу
-        //    });
-        //}
-        //void RemTab(object sender, RoutedEventArgs e)
-        //{
-            
-        //       markofclass.Items.Remove(markofclass.SelectedItem);
-        //    //удаление из базы
-        //}
-
-        //private void btnAdd_Click(object sender, RoutedEventArgs e)
-        //{
-        //    TabItem tab_item = new TabItem();
-        //    markofclass.Items.Add(tab_item);
-
-        //    Label label = new Label();
-        //    label.Content = "New Tab";
-        //    tab_item.Header = label;
-
-        //    Label content = new Label();
-        //    content.Content = "This is the new tab's content";
-        //    tab_item.Content = content;
-        //}
-        //        
-
-
-        //            // Removes the selected tab:  
-        //tabControl1.TabPages.Remove(tabControl1.SelectedTab);  
-        //// Removes all the tabs:  
-        //tabControl1.TabPages.Clear();
-
-
-
-        public class Author
-        {
-            public int ID { get; set; }
-            public string Name { get; set; }
-            public DateTime DOB { get; set; }
-            public string BookTitle { get; set; }
-            public bool IsMVP { get; set; }
-        }
-        private List<Author> LoadCollectionData()
-        {
-            List<Author> authors = new List<Author>();
-            authors.Add(new Author()
-            {
-                ID = 101,
-                Name = "Mahesh Chand",
-                BookTitle = "Graphics Programming with GDI+",
-                DOB = new DateTime(1975, 2, 23),
-                IsMVP = false
-            });
-
-            authors.Add(new Author()
-            {
-                ID = 201,
-                Name = "Mike Gold",
-                BookTitle = "Programming C#",
-                DOB = new DateTime(1982, 4, 12),
-                IsMVP = true
-            });
-
-            authors.Add(new Author()
-            {
-                ID = 244,
-                Name = "Mathew Cochran",
-                BookTitle = "LINQ in Vista",
-                DOB = new DateTime(1985, 9, 11),
-                IsMVP = true
-            });
-
-            return authors;
-            
-        }
+       
        
 
     }
